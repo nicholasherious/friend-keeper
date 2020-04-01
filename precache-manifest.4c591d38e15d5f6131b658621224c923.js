cf2bafdca44ac6c0bb1534afb5865a993d55f9e9
nicholasherious <nherious@gmail.com>
2020-04-01 02:51:56 -0700

    Updates

---
+++ precache-manifest.4c591d38e15d5f6131b658621224c923.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a69e165595e9299d1fcf225cbaf4939",
+    "url": "/friend-keeper/index.html"
+  },
+  {
+    "revision": "9dc843787378ed488e7e",
+    "url": "/friend-keeper/static/css/2.81e2c8a5.chunk.css"
+  },
+  {
+    "revision": "c17e86a343b984185423",
+    "url": "/friend-keeper/static/css/main.dc02a6cc.chunk.css"
+  },
+  {
+    "revision": "9dc843787378ed488e7e",
+    "url": "/friend-keeper/static/js/2.e4e6b904.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/friend-keeper/static/js/2.e4e6b904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c17e86a343b984185423",
+    "url": "/friend-keeper/static/js/main.f714370e.chunk.js"
+  },
+  {
+    "revision": "c2e7f666c60b03be432c",
+    "url": "/friend-keeper/static/js/runtime-main.9199c908.js"
+  }
+]);
